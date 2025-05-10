ff77154c5d2153205f4454c3e5810e2eab81fbab
Zachary Selby <zselby@my.centennialcollege.ca>
2025-05-09 20:09:10 -0400

    added layout and routes

--- src/MainRouter.jsx
+++ src/MainRouter.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
 
 const MainRouter = () => {
     return (
         <div>
+            <Layout />
             <Routes>
                 <Route path="/" element={<h1>Home</h1>} />
+                <Route path="/about" element={<h1>About</h1>} />
+                <Route path="/projects" element={<h1>Projects</h1>} />
+                <Route path="/services" element={<h1>Services</h1>} />
+                <Route path="/contact" element={<h1>Contact</h1>} />
             </Routes>
         </div>
     )
